4dbf2a46a7f6b7bf865e2b7b811107035ee90f3f
Joshua Jonelle Espocia <joshua.espocia@dijgtal.com>
2025-04-01 11:54:17 +0800

    add: chapter 12 - mutating data

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,28 @@
+import Form from "@/app/ui/invoices/edit-form"
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs"
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data"
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params
+	const [invoices, customers] = await Promise.all([
+		fetchInvoiceById(id),
+		fetchCustomers()
+	])
+
+
+	return (
+		<main>
+			<Breadcrumbs
+				breadcrumbs={[
+					{ label: 'Invoices', href: '/dashbaord/invoices' },
+					{
+						label: 'Edit Invoice',
+						href: `/dashboard/invoices/${id}/edit`,
+						active: true,
+					}
+				]}
+			/>
+			<Form invoice={invoices} customers={customers} />
+		</main>
+	)
+}
